17321a2269a13e39df0247e88b9eea9ceb4eec58
LM-official <lorymerc2004@gmail.com>
2025-12-23 17:31:30 +0100

    test color

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -200,10 +200,11 @@ async function renderGraph(graph: HTMLElement, fullSlug: FullSlug) {
     const isCurrent = d.id === slug
 
     // PERSONALIZED COLORING SCHEME
-    if (d.id.includes("tags/hublvl0")) {return "#e82e2e"}
-    if (d.id.includes("tags/hublvl1")) {return "#51d930"}
-    if (d.id.includes("tags/hublvl2")) {return "#3041d9"}
-    if (d.id.includes("tags/hublvl3")) {return "#e0c724"}
+    const hasTag = (tag: string) => d.tags.some(t => t.toLowerCase() === tag.toLowerCase())
+    if (hasTag("hublvl0")) {return "#e82e2e"}
+    if (hasTag("hublvl1")) {return "#51d930"}
+    if (hasTag("hublvl2")) {return "#3041d9"}
+    if (hasTag("hublvl3")) {return "#e0c724"}
 
     if (isCurrent) {
       return computedStyleMap["--secondary"]
